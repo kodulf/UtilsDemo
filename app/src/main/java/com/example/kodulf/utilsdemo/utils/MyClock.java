c118f8b7e08f0460d020ea89aaf6bacbd64277fe
零零漆 <kodulf@500mi.com>
2017-05-25 23:14:50 +0800

    myclock update

--- app/src/main/java/com/example/kodulf/utilsdemo/utils/MyClock.java
+++ app/src/main/java/com/example/kodulf/utilsdemo/utils/MyClock.java
@@ -8,6 +8,8 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Date;
+
 /**
  * Created by Kodulf on 2017/5/25.
  */
@@ -19,6 +21,7 @@ public class MyClock extends View {
     private int radius;
     private int gap;
     private Paint timePaint;
+    private Paint handPaint;
 
     public MyClock(Context context) {
         //super(context);
@@ -47,6 +50,12 @@ public class MyClock extends View {
         timePaint.reset();
         timePaint.setColor(Color.BLACK);
         timePaint.setAntiAlias(true);//设置抗锯齿，使圆形更加圆滑
+
+        handPaint = new Paint();
+        handPaint.reset();
+        handPaint.setColor(Color.RED);
+        handPaint.setAntiAlias(true);
+
     }
 
     @Override
@@ -63,9 +72,66 @@ public class MyClock extends View {
 
         drawCircle(canvas, centerY, centerX);
         drawNumbers(canvas, centerY, centerX);
+        drawPoint(canvas, centerY, centerX);
+
+        Date date = new Date();
+        int hours = date.getHours();
+        int minutes = date.getMinutes();
+        int seconds = date.getSeconds();
+
+        //先画线，然后画中间的小圆圈
 
 
-        //还是从3开始画
+
+        //秒钟
+        handPaint.setStrokeWidth(2);
+        int length = (int)((radius-gap*1.2) * Math.cos((Math.PI*(15-seconds) * 6)/180));
+        int height = (int)((radius-gap*1.2) * Math.sin((Math.PI*(15-seconds) * 6)/180));
+        canvas.drawLine(centerX,centerY,centerX+length,centerY-height,handPaint);
+
+        //分
+        handPaint.setColor(Color.BLUE);
+        handPaint.setStrokeWidth(4);
+        length = (int)((radius-gap*1.5) * Math.cos((Math.PI*(15-minutes) * 6+seconds/10)/180));
+        height = (int)((radius-gap*1.5) * Math.sin((Math.PI*(15-minutes) * 6+seconds/10)/180));
+        canvas.drawLine(centerX,centerY,centerX+length,centerY-height,handPaint);
+
+
+        //小时
+        handPaint.setStrokeWidth(6);
+        length = (int)((radius-gap*2) * Math.cos((Math.PI*(3-hours) * 30+minutes/10)/180));
+        height = (int)((radius-gap*2) * Math.sin((Math.PI*(3-hours) * 30+minutes/10)/180));
+        canvas.drawLine(centerX,centerY,centerX+length,centerY-height,handPaint);
+
+
+        canvas.drawCircle(centerX, centerY,6, mPaint);
+
+    }
+
+    /**
+     * 画刻度
+     * @param canvas
+     * @param centerY
+     * @param centerX
+     */
+    private void drawPoint(Canvas canvas, int centerY, int centerX) {
+        timePaint.setStrokeWidth(3);
+        //还是从3开始画，画每一个小的刻度的时间线
+        for (int i = 0; i < 60; i++) {
+            //需要注意sin，cos 里面的是double 类型的
+            int length = (int)((radius) * Math.cos((Math.PI*i * 6)/180));
+            int height = (int)((radius) * Math.sin((Math.PI*i * 6)/180));
+            Log.d("kodulf","length="+length);
+            Log.d("kodulf","width="+width);
+
+            int length2 = (int)((radius-gap/2) * Math.cos((Math.PI*i * 6)/180));
+            int height2 = (int)((radius-gap/2) * Math.sin((Math.PI*i * 6)/180));
+            canvas.drawLine(centerX+length,centerY-height,centerX+length2,centerY-height2,timePaint);
+        }
+
+        //线加粗
+        timePaint.setStrokeWidth(3);
+        //还是从3开始画，画准点的时间线
         for (int i = 0; i < 12; i++) {
             //需要注意sin，cos 里面的是double 类型的
             int length = (int)((radius) * Math.cos((Math.PI*i * 30)/180));
@@ -73,11 +139,10 @@ public class MyClock extends View {
             Log.d("kodulf","length="+length);
             Log.d("kodulf","width="+width);
 
-            canvas.drawLine(centerX+length,centerY-height,centerX,centerY,timePaint);
-
+            int length2 = (int)((radius-gap) * Math.cos((Math.PI*i * 30)/180));
+            int height2 = (int)((radius-gap) * Math.sin((Math.PI*i * 30)/180));
+            canvas.drawLine(centerX+length,centerY-height,centerX+length2,centerY-height2,timePaint);
         }
-
-
     }
 
     /**
@@ -112,7 +177,7 @@ public class MyClock extends View {
     private void drawCircle(Canvas canvas, int centerY, int centerX) {
 
         canvas.drawCircle(centerX, centerY, radius, mPaint);
-        canvas.drawCircle(centerX, centerY,2, mPaint);
+
     }
 
 }
